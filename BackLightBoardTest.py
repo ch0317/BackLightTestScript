40866e2c2ba872b16689020518b5f5febc82fc9d
ch0317 <304831714@qq.com>
2019-09-17 09:14:55 +0800

    fix 3d cmd

--- BackLightBoardTest.py
+++ BackLightBoardTest.py
@@ -332,8 +332,8 @@ class BackLightTest:
 	def test_BLSET3DCURRENT(self):
 		print("[TEST]:设置3D电流值")
 		cmd_list = ["BLSET2DCURRENT 0\r\n",
-		"BLSET2DCURRENT 30.0\r\n",
-		"BLSET2DCURRENT 10\r\n"]
+		"BLSET3DCURRENT 30.0\r\n",
+		"BLSET3DCURRENT 10\r\n"]
 		for cmd in cmd_list:
 			self.S.write(cmd.encode())
 			j = self.recv()
